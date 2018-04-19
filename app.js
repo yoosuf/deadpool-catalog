0cfe4f20e1be083a7b168aabbac4ffc9c8d1b3e1
Yoosuf <mayoosuf@gmail.com>
2018-04-19 17:04:57 +0530

    updated

--- app.js
+++ app.js
@@ -2,16 +2,17 @@ var express = require('express');
 var path = require('path');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
+var cors = require('cors');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
