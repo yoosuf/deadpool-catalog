9f75a322a38e390cc17eae01ec5c60270d70ae0d
Yoosuf <mayoosuf@gmail.com>
2018-05-30 15:03:41 +0530

    updated

--- src/controllers/calculateController.js
+++ src/controllers/calculateController.js
@@ -6,13 +6,9 @@ const converter = require('google-currency')
 
 // Display list of all Providers.
 exports.calculate = function (req, res) {
-    // console.log(req);
-
-
     if (req.query.buy_value === undefined || req.query.sell_value === undefined || req.query.amount === undefined) {
-        res.json({error: false, message: 'NOT COMPLETE: Request data not complete'});
+        res.json({error: true, message: 'NOT COMPLETE: Request data not complete'}, 400);
     }
-
     const val = (parseFloat(req.query.amount) / parseFloat(req.query.buy_value)) * parseFloat(req.query.sell_value);
 
     const options = {
@@ -20,27 +16,16 @@ exports.calculate = function (req, res) {
         to: req.query.buy_currency,
         amount: val
     };
-
-
     console.log(`Calculated Val ${val}`)
-
-
     converter(options).then(value => {
-
         console.log(`google Val ${value.converted}`)
-
         console.log(`SRC Val ${req.query.amount}`)
-
         let calculatedVal =  value.converted - req.query.amount;
-
         console.log(`results Val ${calculatedVal}`)
-
-        res.json({"data": {"value": calculatedVal}});
-
+        res.json({ error: false, "data": {"value": calculatedVal}}, 200);
     }).catch(e => {
-        res.json({"Error": {"error": e}})
-
-});
+        res.json({error: true, message: e});
+    });
 
 
 };
