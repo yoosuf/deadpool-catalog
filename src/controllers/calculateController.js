0406e3efa75eb94dcdbdb44bfbfaf8cc3ce65f4e
Yoosuf <mayoosuf@gmail.com>
2018-05-30 14:30:33 +0530

    updated

--- src/controllers/calculateController.js
+++ src/controllers/calculateController.js
@@ -1,16 +1,49 @@
 // var Provider = require('../models/provider');
-var coinbase = require('../providers/coinbase');
-var kraken = require('../providers/kraken');
+const coinbase = require('../providers/coinbase');
+const kraken = require('../providers/kraken');
+const converter = require('google-currency')
+
 
 // Display list of all Providers.
-exports.calculate = function(req, res) {
+exports.calculate = function (req, res) {
     // console.log(req);
 
-    if(req.query.buy_value == undefined || req.query.sell_value == undefined || req.query.amount == undefined)
-        res.send('NOT COMPLETE: Request data not complete');
-    
-    var val =  (parseFloat(req.query.amount) / parseFloat(req.query.buy_value)) * parseFloat(req.query.sell_value)
-    console.log(val);
 
-    res.send({"data": {"value" : val}});
+    if (req.query.buy_value === undefined || req.query.sell_value === undefined || req.query.amount === undefined) {
+        res.json({error: false, message: 'NOT COMPLETE: Request data not complete'});
+    }
+
+    const val = (parseFloat(req.query.amount) / parseFloat(req.query.buy_value)) * parseFloat(req.query.sell_value);
+
+    const options = {
+        from: req.query.sell_currency,
+        to: req.query.buy_currency,
+        amount: val
+    };
+
+
+    console.log(`Calculated Val ${val}`)
+
+
+    converter(options).then(value => {
+
+        console.log(`google Val ${value.converted}`)
+
+        console.log(`SRC Val ${req.query.amount}`)
+
+        let calculatedVal =  value.converted - req.query.amount;
+
+        console.log(`results Val ${calculatedVal}`)
+
+        res.json({"data": {"value": calculatedVal}});
+
+    }).catch(e => {
+        res.json({"Error": {"error": e}})
+
+});
+
+
 };
+
+
+
--- src/helpers/helper.js
+++ src/helpers/helper.js
@@ -1,5 +1,54 @@
+var https = require('https');
+
+
 export function superScript(string) {
     return string;
 }
 
 
+//http://data.fixer.io/api/convert
+//     ? access_key = API_KEY
+//     & from = GBP
+//     & to = JPY
+//     & amount = 25
+
+export function convertCurrency(fromCurrency, toCurrency, amount) {
+
+    var apiKey = 'access_key=2a052e5ae8296c3dc37062b4a2c39be8';
+    fromCurrency = encodeURIComponent(fromCurrency);
+    toCurrency = encodeURIComponent(toCurrency);
+    var query = `&from=${fromCurrency}&to=${toCurrency}&amount=${amount}`;
+    var url = 'http://data.fixer.io/api/convert?' + apiKey + query;
+
+    console.log(url);
+
+    https.get(url, function (res) {
+        var body = '';
+
+        res.on('data', function (chunk) {
+            body += chunk;
+        });
+
+        res.on('end', function () {
+            try {
+                var jsonObj = JSON.parse(body);
+
+                var val = jsonObj[query];
+                if (val) {
+                    var total = val * amount;
+                    cb(null, Math.round(total * 100) / 100);
+                } else {
+                    var err = new Error("Value not found for " + query);
+                    console.log(err);
+                    cb(err);
+                }
+            } catch (e) {
+                console.log("Parse error: ", e);
+                cb(e);
+            }
+        });
+    }).on('error', function (e) {
+        console.log("Got an error: ", e);
+        cb(e);
+    });
+}
