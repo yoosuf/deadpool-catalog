d5a0af5dc79bc683446a57c80cc2a219da704199
Yoosuf <mayoosuf@gmail.com>
2018-07-24 18:19:58 +0530

    wip adidng admin auth and react client for admin

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -70,9 +70,10 @@ $app->middleware([
     Spatie\Cors\Cors::class,
 ]);
 
-// $app->routeMiddleware([
+$app->routeMiddleware([
 //     'auth' => App\Http\Middleware\Authenticate::class,
-// ]);
+    'jwt.auth' => App\Http\Middleware\JwtMiddleware::class,
+]);
 
 /*
 |--------------------------------------------------------------------------
