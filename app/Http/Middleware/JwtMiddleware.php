e535750804ea2e081cee28718734f1e1085ed2dc
Yoosuf <mayoosuf@gmail.com>
2018-07-30 22:13:26 +0530

    changes

--- app/Http/Middleware/JwtMiddleware.php
+++ app/Http/Middleware/JwtMiddleware.php
@@ -16,18 +16,27 @@ class JwtMiddleware
         if(!$token) {
             // Unauthorized response if token not there
             return response()->json([
-                'error' => 'Token not provided.'
+                'error' => [
+                    'code' => 401, 
+                    'message' => 'Token not provided.'
+                ]
             ], 401);
         }
         try {
             $credentials = JWT::decode($token, env('JWT_SECRET'), ['HS256']);
         } catch(ExpiredException $e) {
             return response()->json([
-                'error' => 'Provided token is expired.'
+                'error' => [
+                    'code' => 400, 
+                    'message' => 'Provided token is expired.'
+                ]
             ], 400);
         } catch(Exception $e) {
             return response()->json([
-                'error' => 'An error while decoding token.'
+                'error' => [
+                    'code' => 400, 
+                    'message' => 'An error while decoding token.'
+                ]
             ], 400);
         }
         $user = User::find($credentials->sub);
