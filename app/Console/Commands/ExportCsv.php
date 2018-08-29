9202fdbb833a1921455301400608ec66412f3d02
Prabuddha <prabuddhanipun@gmail.com>
2018-08-29 11:59:24 +0530

    csv export job

--- app/Console/Commands/ExportCsv.php
+++ app/Console/Commands/ExportCsv.php
@@ -4,15 +4,9 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Http\Response;
-use App\Http\Controllers\FIltersController;
 use App\Jobs\ProcessExportCsv;
 use Carbon\Carbon;
 use Log;
-use DB;
-use Illuminate\Support\Facades\Mail;
-use App\Mail\SendMailable;
-use App\Events\SendMailEvent;
-
 
 class ExportCsv extends Command
 {
@@ -39,7 +33,6 @@ class ExportCsv extends Command
      */
     public function __construct()
     {
-        $this->filter = new FIltersController();
         parent::__construct();
     }
 
@@ -50,230 +43,12 @@ class ExportCsv extends Command
      */
     public function handle()
     {
-        $amount = 100;
+        Log::info("ExportCsv Queues Begins");
 
-        $finalBuyArr = array();
-        $finalSellArr = array();
-       
+        //ProcessExchanges::dispatch();
 
-        $historicalBuyData = DB::table("exchange_logs")
-                ->whereDate('created_at', '>', Carbon::now()->subDays(45))
-                ->get();
+        dispatch(new ProcessExportCsv());
 
-        $historicalSellData = DB::table("exchange_logs")
-                ->whereDate('created_at', '>', Carbon::now()->subDays(45))
-                ->get();
-
-       // print_r($historicalBuyData);exit;
-
-
-        foreach ($historicalBuyData as $k => $val)
-        {
-            $buyArr = array();
-            $exchangeArr = json_decode($val->preference);
-
-            foreach ($exchangeArr->rates as $key => $value) 
-            {
-                // if($key == $fromCurrencyArr)
-                // {
-                    foreach ($value as $crypto => $data)
-                    {
-                       // if($cryptoCurrency == 'all') {
-
-                            if($data->buydata != 0)
-                            {
-                                $buyPrice = $data->buydata;
-
-                                $buyArr['price'] = $data->buydata;
-                                $buyArr['base'] = $data->base;
-                                $buyArr['currency'] = $data->currency;
-                                $buyArr['name'] = $exchangeArr->name;
-                                $buyArr['timestamp'] = date('Y-m-d H:i', strtotime($val->created_at));
-
-                                $finalBuyArr[] = $buyArr;
-                            }
-                        //}
-                    }
-               // }
-            }
-        }
-
-        
-
-
-        foreach ($historicalSellData as $k => $val)
-        {
-           // echo $k;
-            $sellArr = array();
-            $exchangeArr = json_decode($val->preference);
-
-            foreach ($exchangeArr->rates as $key => $value) 
-            {
-                // if($key == $fromCurrencyArr)
-                // {
-                    foreach ($value as $crypto => $data)
-                    {
-                        //if($cryptoCurrency == 'all') {
-
-                            if($data->selldata != 0)
-                            {
-                                $sellPrice = $data->selldata;
-                                $sellArr['price'] = $data->selldata;
-                                $sellArr['base'] = $data->base;
-                                $sellArr['currency'] = $data->currency;
-                                $sellArr['name'] = $exchangeArr->name;
-                                $sellArr['timestamp'] = date('Y-m-d H:i', strtotime($val->created_at));
-
-                                $finalSellArr[] = $sellArr;
-
-                            }
-                        //}
-                    }   
-                //}
-            }
-        }
-
-        $currency_layer = $this->filter->getCurrencyLayerData();
-
-        // print_r($currency_layer);
-      // print_r($finalSellArr);exit;
-
-        // Set response headers to trigger file download on client side
-        header("Content-type: application/csv");
-        header("Content-Disposition: attachment;filename=exchange_logs.csv");
-
-
-        $filename = "./storage/csv/logins-".time().".csv";
-
-
-        $output_file_pointer = fopen($filename, 'w');
-
-        
-   // $handle = fopen($filename, 'w');
-
-        // // Output the CSV headers
-        $headers = array(
-            'Timestamp',
-            'Sending exchange',
-            'Receiving Exchange',
-            'Sending currency',
-            'Receiving currency',
-            'Sending crypto',
-            'Receiving crypto',
-            'Sending price',
-            'Receiving price',
-            'Profit'
-        );
-
-        //print_r($finalBuyArr);exit;
-
-        fputcsv($output_file_pointer, $headers);
-            
-        foreach ($finalBuyArr as $k1 => $buydata) {
-
-            $buybase = $buydata['base'];
-            $buyExchange = $buydata['name'];  
-            $buyCurr = $buydata['currency'];
-            $buyTime = $buydata['timestamp'];
-
-
-            foreach ($finalSellArr as $k2 => $selldata) {
-
-
-                $sellExchange = $selldata['name'];
-                $sellbase = $selldata['base'];
-                $selcurr = $selldata['currency'];
-                $sellTime = $selldata['timestamp'];
-
-                //echo $sellExchange.'/';
-
-                foreach ($currency_layer as $id => $value) 
-                {
-                    if($value->iso == $buyCurr)
-                    {
-                        $ratesArr = json_decode($value->other_conversion_values);
-                    }
-                }
-
-                if($buybase == $sellbase AND $buyTime == $sellTime)
-                {
-                //echo $sellExchange;
-                // exit;
-                    $keystr = $buyCurr.$selcurr;
-                    $rate = $ratesArr->data->$keystr;
-
-    
-                    $val = (floatval($amount) / floatval($buydata['price'])) * floatval($selldata['price']);
-                    $convertedVal = $val/$rate;
-                    $calculatedVal = $convertedVal - $amount;
-
-                    $percentage =  ($calculatedVal/$amount)*100;
-
-                    $output = array(
-
-                        $buydata['timestamp'],
-                        $buyExchange,
-                        $sellExchange,
-                        $buyCurr,
-                        $selcurr,
-                        $buybase,
-                        $sellbase,
-                        $buydata['price'],
-                        $selldata['price'],
-                        number_format((float)$percentage, 2, '.', '')
-                        // $val['created_at']
-                    );
-                    
-                    fputcsv($output_file_pointer, $output);
-                    // ob_flush();
-                    // flush();
-
-                }
-
-            }
-
-        }
-
-        // $headers = array(
-
-        //     'Content-type: application/csv',
-        //     'Content-Disposition: attachment;filename=exchange_logs.csv'
-
-        // );
-
-        // $file= "logins.csv";
-
-        // $headers = array(
-        //           'Content-type: application/csv',
-        //         );
-    
-        // return response()->download($file, 'test.csv', $headers);
-
-
-
-        fclose($output_file_pointer);
-
-
-        exit;
-
-        $response = [
-
-            'url'=>'fgghf/ddd/dddsdasd.ff',
-            'status'=>'success'
-        ];
-
-        event(new SendMailEvent($response));
-
-        //Mail::to('welltech90@yopmail.com')->send(new SendMailable(34));
-
-       // Mail::raw('Raw string email', function($msg) { $msg->to(['welltech90@yopmail.com']); $msg->from(['test@test.com']); });
-       //print_r($x);exit;
-
-
-        // return response()->download($pathToFile, 'test.csv', $headers);
-
-        //return response()->download('/../logins.csv');
-
-        die;
+        Log::info("ExportCsv Queues Ends");
     }
 }
--- app/Jobs/ProcessExportCsv.php
+++ app/Jobs/ProcessExportCsv.php
@@ -2,9 +2,14 @@
 
 namespace App\Jobs;
 
-use DB;
-//use App\ExchangeLog;
 
+use App\Http\Controllers\FIltersController;
+use Carbon\Carbon;
+use Log;
+use DB;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\SendMailable;
+use App\Events\SendMailEvent;
 
 class ProcessExportCsv extends Job
 {
@@ -15,7 +20,7 @@ class ProcessExportCsv extends Job
      */
     public function __construct()
     {
-        //
+        $this->filter = new FIltersController();
     }
 
     /**
@@ -25,186 +30,189 @@ class ProcessExportCsv extends Job
      */
     public function handle()
     {
-        echo 'hello';exit;
-            $amount = 100;
+        $amount = 100;
 
-            $finalBuyArr = array();
-            $finalSellArr = array();
-           
+        $finalBuyArr = array();
+        $finalSellArr = array();
+       
 
-            $historicalBuyData = DB::table("exchange_logs")
-                    ->whereDate('created_at', '>', Carbon::now()->subDays(45))
-                    ->get();
+        $historicalBuyData = DB::table("exchange_logs")
+                ->whereDate('created_at', '>', Carbon::now()->subDays(45))
+                ->get();
 
-            $historicalSellData = DB::table("exchange_logs")
-                    ->whereDate('created_at', '>', Carbon::now()->subDays(45))
-                    ->get();
+        $historicalSellData = DB::table("exchange_logs")
+                ->whereDate('created_at', '>', Carbon::now()->subDays(45))
+                ->get();
 
-           // print_r($historicalBuyData);exit;
+       // print_r($historicalBuyData);exit;
 
 
-            foreach ($historicalBuyData as $k => $val)
+        foreach ($historicalBuyData as $k => $val)
+        {
+            $buyArr = array();
+            $exchangeArr = json_decode($val->preference);
+
+            foreach ($exchangeArr->rates as $key => $value) 
             {
-                $buyArr = array();
-                $exchangeArr = json_decode($val->preference);
-    
-                foreach ($exchangeArr->rates as $key => $value) 
-                {
-                    // if($key == $fromCurrencyArr)
-                    // {
-                        foreach ($value as $crypto => $data)
-                        {
-                           // if($cryptoCurrency == 'all') {
-    
-                                if($data->buydata != 0)
-                                {
-                                    $buyPrice = $data->buydata;
-    
-                                    $buyArr['price'] = $data->buydata;
-                                    $buyArr['base'] = $data->base;
-                                    $buyArr['currency'] = $data->currency;
-                                    $buyArr['name'] = $exchangeArr->name;
-                                    $buyArr['timestamp'] = date('Y-m-d H:i', strtotime($val->created_at));
-    
-                                    $finalBuyArr[] = $buyArr;
-                                }
-                            //}
-                        }
-                   // }
-                }
-            }
-
-            
-
-
-            foreach ($historicalSellData as $k => $val)
-            {
-               // echo $k;
-                $sellArr = array();
-                $exchangeArr = json_decode($val->preference);
-
-                foreach ($exchangeArr->rates as $key => $value) 
-                {
-                    // if($key == $fromCurrencyArr)
-                    // {
-                        foreach ($value as $crypto => $data)
-                        {
-                            //if($cryptoCurrency == 'all') {
-
-                                if($data->selldata != 0)
-                                {
-                                    $sellPrice = $data->selldata;
-                                    $sellArr['price'] = $data->selldata;
-                                    $sellArr['base'] = $data->base;
-                                    $sellArr['currency'] = $data->currency;
-                                    $sellArr['name'] = $exchangeArr->name;
-                                    $sellArr['timestamp'] = date('Y-m-d H:i', strtotime($val->created_at));
-
-                                    $finalSellArr[] = $sellArr;
-
-                                }
-                            //}
-                        }   
-                    //}
-                }
-            }
-
-            $currency_layer = $this->filter->getCurrencyLayerData();
-
-            // print_r($currency_layer);
-          // print_r($finalSellArr);exit;
-
-            // Set response headers to trigger file download on client side
-
-            // header('Content-disposition: attachment; filename=file.json');
-            // header('Content-type: application/json');
-
-            $output = [];
-          
-           // fputcsv($output_file_pointer, $headers);
-                
-            foreach ($finalBuyArr as $k1 => $buydata) {
-
-                $buybase = $buydata['base'];
-                $buyExchange = $buydata['name'];  
-                $buyCurr = $buydata['currency'];
-                $buyTime = $buydata['timestamp'];
-
-
-                foreach ($finalSellArr as $k2 => $selldata) {
-
-
-                    $sellExchange = $selldata['name'];
-                    $sellbase = $selldata['base'];
-                    $selcurr = $selldata['currency'];
-                    $sellTime = $selldata['timestamp'];
-
-                    //echo $sellExchange.'/';
-    
-                    foreach ($currency_layer as $id => $value) 
+                // if($key == $fromCurrencyArr)
+                // {
+                    foreach ($value as $crypto => $data)
                     {
-                        if($value->iso == $buyCurr)
-                        {
-                            $ratesArr = json_decode($value->other_conversion_values);
-                        }
+                       // if($cryptoCurrency == 'all') {
+
+                            if($data->buydata != 0)
+                            {
+                                $buyPrice = $data->buydata;
+
+                                $buyArr['price'] = $data->buydata;
+                                $buyArr['base'] = $data->base;
+                                $buyArr['currency'] = $data->currency;
+                                $buyArr['name'] = $exchangeArr->name;
+                                $buyArr['timestamp'] = date('Y-m-d H:i', strtotime($val->created_at));
+
+                                $finalBuyArr[] = $buyArr;
+                            }
+                        //}
                     }
-
-                    if($buybase == $sellbase AND $buyTime == $sellTime)
-                    {
-                    //echo $sellExchange;
-                    // exit;
-                        $keystr = $buyCurr.$selcurr;
-                        $rate = $ratesArr->data->$keystr;
+               // }
+            }
+        }
 
         
-                        $val = (floatval($amount) / floatval($buydata['price'])) * floatval($selldata['price']);
-                        $convertedVal = $val/$rate;
-                        $calculatedVal = $convertedVal - $amount;
 
-                        $percentage =  ($calculatedVal/$amount)*100;
 
-                        $output[] = array(
+        foreach ($historicalSellData as $k => $val)
+        {
+           // echo $k;
+            $sellArr = array();
+            $exchangeArr = json_decode($val->preference);
 
-                            'Timestamp'=> $buydata['timestamp'],
-                            'Sending exchange'=> $buyExchange,
-                            'Receiving Exchange'=> $sellExchange,
-                            'Sending currency'=>$buyCurr,
-                            'Receiving currency'=>$selcurr,
-                            'Sending crypto'=>$buybase,
-                            'Receiving crypto'=>$sellbase,
-                            'Sending price'=>$buydata['price'],
-                            'Receiving price'=>$selldata['price'],
-                            'Profit'=>number_format((float)$percentage, 2, '.', '')
-                            // $val['created_at']
-                        );
-                        
-                        // fputcsv($output_file_pointer, $output);
-                        // ob_flush();
-                        // flush();
+            foreach ($exchangeArr->rates as $key => $value) 
+            {
+                // if($key == $fromCurrencyArr)
+                // {
+                    foreach ($value as $crypto => $data)
+                    {
+                        //if($cryptoCurrency == 'all') {
 
+                            if($data->selldata != 0)
+                            {
+                                $sellPrice = $data->selldata;
+                                $sellArr['price'] = $data->selldata;
+                                $sellArr['base'] = $data->base;
+                                $sellArr['currency'] = $data->currency;
+                                $sellArr['name'] = $exchangeArr->name;
+                                $sellArr['timestamp'] = date('Y-m-d H:i', strtotime($val->created_at));
+
+                                $finalSellArr[] = $sellArr;
+
+                            }
+                        //}
+                    }   
+                //}
+            }
+        }
+
+        $currency_layer = $this->filter->getCurrencyLayerData();
+
+        // print_r($currency_layer);
+      // print_r($finalSellArr);exit;
+
+        // Set response headers to trigger file download on client side
+        header("Content-type: application/csv");
+        header("Content-Disposition: attachment;filename=exchange_logs.csv");
+
+
+        $filename = "./storage/csv/logins-".time().".csv";
+
+
+        $output_file_pointer = fopen($filename, 'w');
+
+        
+   // $handle = fopen($filename, 'w');
+
+        // // Output the CSV headers
+        $headers = array(
+            'Timestamp',
+            'Sending exchange',
+            'Receiving Exchange',
+            'Sending currency',
+            'Receiving currency',
+            'Sending crypto',
+            'Receiving crypto',
+            'Sending price',
+            'Receiving price',
+            'Profit'
+        );
+
+        //print_r($finalBuyArr);exit;
+
+        fputcsv($output_file_pointer, $headers);
+            
+        foreach ($finalBuyArr as $k1 => $buydata) {
+
+            $buybase = $buydata['base'];
+            $buyExchange = $buydata['name'];  
+            $buyCurr = $buydata['currency'];
+            $buyTime = $buydata['timestamp'];
+
+
+            foreach ($finalSellArr as $k2 => $selldata) {
+
+
+                $sellExchange = $selldata['name'];
+                $sellbase = $selldata['base'];
+                $selcurr = $selldata['currency'];
+                $sellTime = $selldata['timestamp'];
+
+                //echo $sellExchange.'/';
+
+                foreach ($currency_layer as $id => $value) 
+                {
+                    if($value->iso == $buyCurr)
+                    {
+                        $ratesArr = json_decode($value->other_conversion_values);
                     }
-
                 }
 
+                if($buybase == $sellbase AND $buyTime == $sellTime)
+                {
+                //echo $sellExchange;
+                // exit;
+                    $keystr = $buyCurr.$selcurr;
+                    $rate = $ratesArr->data->$keystr;
+
+    
+                    $val = (floatval($amount) / floatval($buydata['price'])) * floatval($selldata['price']);
+                    $convertedVal = $val/$rate;
+                    $calculatedVal = $convertedVal - $amount;
+
+                    $percentage =  ($calculatedVal/$amount)*100;
+
+                    $output = array(
+
+                        $buydata['timestamp'],
+                        $buyExchange,
+                        $sellExchange,
+                        $buyCurr,
+                        $selcurr,
+                        $buybase,
+                        $sellbase,
+                        $buydata['price'],
+                        $selldata['price'],
+                        number_format((float)$percentage, 2, '.', '')
+                        // $val['created_at']
+                    );
+                    
+                    fputcsv($output_file_pointer, $output);
+                    
+
+                }
             }
+        }
 
-            //fclose($output_file_pointer);
-
-           $json = json_encode($output);
-            
-
-
-            echo $json;
-
-            
-
-            // $fp = fopen('results.json', 'w');
-            // fwrite($fp, json_encode($output));
-            // fclose($fp);
-
-            //die;
-       // }
-
+        fclose($output_file_pointer);
+        exit;
     }
 
     public function failed(Exception $exception)
